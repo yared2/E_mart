b5d24ad658de9ed6c621672d69384a156a9726b1
aagezai <aagezai@gmail.com>
2020-06-14 22:38:13 -0700

    no message

--- src/main/java/edu/miu/e_mart/domain/CartItem.java
+++ src/main/java/edu/miu/e_mart/domain/CartItem.java
@@ -1,33 +1,34 @@
-package edu.miu.e_mart.domain;
+/*package edu.miu.e_mart.domain;
 
 import javax.persistence.*;
 
 
-/**
+*//**
  * This class is A CartItem which has a product and quantity of product
- */
+ *//*
+
 @Entity
 public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer itemId;
-    /**
+    *//**
      * a CartItem has a product
-     */
+     *//*
     @OneToOne
     private Product product;
-    /**
+    *//**
      * number of prduct
-     */
+     *//*
     private int itemQuantity;
 
     public Integer getItemId() {
         return itemId;
     }
 
-    /**
+    *//**
      * The no-args constructor
-     */
+     *//*
     public CartItem() {
     }
 
@@ -46,4 +47,5 @@ public class CartItem {
     public void setItemQuantity(int itemQuantity) {
         this.itemQuantity = itemQuantity;
     }
-}
+} */
+*/
