2b3c050e8e0457b84c01ac52498babc0b95260ee
wintesfamariam <wintesfamariam@gmail.com>
2020-06-14 22:50:50 -0500

    review and payment features updated

--- src/main/java/edu/miu/e_mart/controller/LoginController.java
+++ src/main/java/edu/miu/e_mart/controller/LoginController.java
@@ -5,18 +5,18 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import edu.miu.e_mart.domain.Credential;
+//import edu.miu.e_mart.domain.Credential;
 
 @Controller
 public class LoginController {
-	@RequestMapping(value= {"/login"}, method = RequestMethod.GET)
-	public String getLoginPage(@ModelAttribute("credential")Credential credential) {
-		return "login/login";
-	}
-	
-	@RequestMapping(value= {"/login"}, method = RequestMethod.POST)
-	public String login(@ModelAttribute("credential")Credential credential) {
-		return "products";
-	}
+//	@RequestMapping(value= {"/login"}, method = RequestMethod.GET)
+//	public String getLoginPage(@ModelAttribute("credential")Credential credential) {
+//		return "login/login";
+//	}
+//
+//	@RequestMapping(value= {"/login"}, method = RequestMethod.POST)
+//	public String login(@ModelAttribute("credential")Credential credential) {
+//		return "products";
+//	}
 
 }
